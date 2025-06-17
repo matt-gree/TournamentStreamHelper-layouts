a8bd06528a110e2d416266305297fc9009250e77
matt-gree <m10greene@gmail.com>
2025-06-16 21:34:41 -0500

    added team logo html files

---
+++ msb_teams/teamlogo.js
@@ -0,0 +1,24 @@
+LoadEverything().then(() => {
+  Start = async () => {};
+
+  Update = async (event) => {
+    let data = event.data;
+
+    const path = window.location.pathname;
+    const page = path.split("/").pop();
+    const teamNum = (page === "teamlogo2.html") ? 2 : 1;
+
+    const logoContainer = document.getElementById("team-logo-container");
+    logoContainer.innerHTML = "";  // Clear previous logo
+
+    if ('score' in data) {
+      const playerData = data.score['1'].team[`${teamNum}`].player['1'];
+      if (playerData && playerData.msb_team) {
+        const teamIcon = document.createElement("img");
+        teamIcon.src = `../../assets/rio_teamLogos/${playerData.msb_team}.png`;
+        teamIcon.className = "team-logo";
+        logoContainer.appendChild(teamIcon);
+      }
+    }
+  };
+});
